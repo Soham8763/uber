3a8c5c6947d7206d233acbdb253e22772fae0f8e
Soham Ghosh <soham@Sohams-MacBook-Air.local>
2025-07-17 00:08:56 +0530

    basic frontend setup

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div>
+        <div className='bg-cover bg-center bg-[url(https://images.unsplash.com/photo-1624724126923-e2c021df1311?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] flex pt-8 justify-between flex-col w-full h-screen'>
+            <img className='w-16 ml-8' src="https://imgs.search.brave.com/mqZ2TMeO2R6hJQCG1z0AaA9OxbbmsB5ydQ67hFv0At0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9mcmVl/bG9nb3BuZy5jb20v/aW1hZ2VzL2FsbF9p/bWcvMTY1OTc2MTEw/MHViZXItbG9nby1w/bmcucG5n" alt="" />
+            <div className='bg-white py-4 pb-7 px-4'>
+                <h2 className='text-3xl font-bold'>Get Started with Uber</h2>
+                <Link to='/login' className='flex items-center justify-center w-full bg-black text-white py-3 rounded mt-5'>Continue</Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
