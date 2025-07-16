610a0fc7f51ceda1c7d9768e88a91b8281912696
Soham Ghosh <soham@Sohams-MacBook-Air.local>
2025-07-16 15:20:44 +0530

    register route created

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,24 @@
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 dotenv.config();
-const express = require("express")
-const cors = require('cors')
-const app = express()
+
+const express = require("express");
+const app =express();
+
+const cors = require('cors');
 app.use(cors());
-app.get('/',(req,res)=>{
-  res.send("Hello World")
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+const connectToDb = require('./db/db')
+connectToDb();
+
+const userRoutes = require('./routes/user.routes')
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
 })
+
+app.use('/users',userRoutes);
 
 module.exports = app;
